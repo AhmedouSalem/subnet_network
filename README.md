8c7131cb4f5467c8ca7f370d1e6afe9b053c05aa
AhmedouSalem <ahmedou8salem@gmail.com>
2024-02-24 08:52:41 +0100

    Retoucher la logique

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # subnet_network
 Un projet JS permettant le decoupage d'un adresse IP en plusieurs sous adresses réseaux
 # Steps1
+
+# REMOVED
+
+
+        // Collect data for Subnetting - Postes
+        // var postInputs = document.getElementsByName("post[]");
+        // var postData = [];
+
+        // for (var j = 0; j < postInputs.length; j++) {
+        //     postData.push(postInputs[j].value);
+        // }
+
+        // console.log("Subnetting Data - Posts:", postData);
+
+        // Ajoutez ici la logique pour le cas de Subnetting
--- js/local/script.js
+++ js/local/script.js
@@ -15,16 +15,10 @@ function loadOptions() {
   
         <label for="subnets">Nombre de sous-réseaux :</label>
         <input type="number" id="subnets" name="subnets" placeholder="Entrez le nombre de sous-réseaux">
-<<<<<<< HEAD
         <button type="button" onclick="collectAndProcessData()">Terminate</button>
       `;
     } else if (selectedOption === "vlsm") {
         i = 1;
-=======
-        <button type="button" onclick="">Terminate</button>
-      `;
-    } else if (selectedOption === "vlsm") {
->>>>>>> 44e57f8df75d9eb117dddd07ed87ff8b903eed5e
         dynamicInputsDiv.innerHTML = `
         <label for="ipAddress">Adresse IP :</label>
         <input type="text" id="ipAddress" name="ipAddress" placeholder="Entrez l'adresse IP">
@@ -34,24 +28,16 @@ function loadOptions() {
         <input type="text" name="subnet[]" placeholder="Nombre des machines">
         </div>
   
-<<<<<<< HEAD
         <button type="button" onclick="addPostInput()">+</button>  
         <button type="button" onclick="collectAndProcessData()">Terminate</button>
       `;
         i++
-=======
-        <button type="button" onclick="addVLSMInput()">+</button>  
-        <button type="button" onclick="">Terminate</button>
-      `;
-    i++
->>>>>>> 44e57f8df75d9eb117dddd07ed87ff8b903eed5e
     }// Add additional condition to clear content for "Select method"
     else if (selectedOption === "selectMethod") {
         // Do nothing, leave dynamicInputsDiv empty
     }
 }
 
-<<<<<<< HEAD
 // ajouter des postes
 function addPostInput() {
     var vlsmInputsDiv = document.getElementById("vlsmInputs");
@@ -78,18 +64,6 @@ function collectAndProcessData() {
 
         // Collect data for Subnetting
         console.log("Subnetting Data - IP:", ipAddress, "Subnets:", subnets);
-
-        // Collect data for Subnetting - Postes
-        var postInputs = document.getElementsByName("post[]");
-        var postData = [];
-
-        for (var j = 0; j < postInputs.length; j++) {
-            postData.push(postInputs[j].value);
-        }
-
-        console.log("Subnetting Data - Posts:", postData);
-
-        // Ajoutez ici la logique pour le cas de Subnetting
     } else if (selectedOption === "vlsm") {
         var vlsmInputs = document.getElementsByName("subnet[]");
         var vlsmData = [];
@@ -103,14 +77,3 @@ function collectAndProcessData() {
         // Ajoutez ici la logique pour le cas de VLSM
     }
 }
-=======
-function addVLSMInput() {
-    var vlsmInputsDiv = document.getElementById("vlsmInputs");
-    vlsmInputsDiv.innerHTML += `
-      <label for="subnet">Poste${i} :</label>
-      <input type="text" name="subnet[]" placeholder="Nombre des machines">
-      <br>
-    `;
-    i++;
-}
->>>>>>> 44e57f8df75d9eb117dddd07ed87ff8b903eed5e
